7c19b93266a020fc2864ec0b49aa0c88b200a224
DevJG <andrew.georgiou03@gmail.com>
2024-01-30 19:52:09 +0000

    Greyscale filter complete

--- Src/Filters/greyscale.js
+++ Src/Filters/greyscale.js
@@ -1,18 +1,21 @@
-// class GreyscaleFilter
-// {
-//     constructor()
-//     {
-
-//     }
-// }
-
 function GreyscaleFilter(_img)
 {
+    _img.loadPixels();
     for (let y = 0; y < _img.height; ++y)
     {
         for (let x = 0; x < _img.width; ++x)
         {
-            // TODO: greyscale image here. try set it first without needing to return it because js might do it by reference instead of value
+            const index = (x + y * _img.width) * 4;
+            let average = ((_img.pixels[index + 0] + _img.pixels[index + 1] + _img.pixels[index + 2]) / 3) * 1.2;
+
+            if (average > 255)
+                average = 255; 
+
+            _img.pixels[index + 0] = average;
+            _img.pixels[index + 1] = average;
+            _img.pixels[index + 2] = average;
+            _img.pixels[index + 3] = 255;
         }
     }
+    _img.updatePixels();
 }
--- Src/Grid/cell.js
+++ Src/Grid/cell.js
@@ -1,39 +1,18 @@
-const scale = 1.6;
-
-class GridManager
-{
-    static s_Grid = [];
-    static s_Width = 160 * scale;
-    static s_Height = 120 * scale;
-
-    static addCell(_cell)
-    {
-        this.s_Grid.push(_cell);
-    }
-
-    static drawCells()
-    {
-        for (let i = 0; i < this.s_Grid.length; ++i)
-        {
-            const x = (i * this.s_Width) % width;
-            const y = Math.floor(i * this.s_Width / width) * this.s_Height;
-
-            this.s_Grid[i].draw(x, y, this.s_Width, this.s_Height);
-        }
-    }
-}
-
 class Cell
 {
     constructor(_img, _filter)
     {
         this.background = color(random(255), random(255), random(255));
-        this.img = _img;
-        this.filter = _filter;
+        this.img = _img.get();
+
+        if (_filter !== undefined)
+            _filter(this.img);
     }
 
     draw(_x, _y, _width, _height)
     {
-        image(img, _x, _y, _width, _height);
+        image(this.img, _x, _y, _width, _height);
     }
 }
+
+const filters = [() => {}, GreyscaleFilter];
