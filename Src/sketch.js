680ae07a24a74962f1bfad3f52c6792c700a8cf2
DevJG <andrew.georgiou03@gmail.com>
2024-01-30 20:39:21 +0000

    Channel mask filters complete

--- Src/sketch.js
+++ Src/sketch.js
@@ -8,7 +8,9 @@ function preload()
 function setup()
 {
     createCanvas(1280, 720);
+    pixelDensity(1);
 
+    // img.resize(160, 120); // TODO: potentially switch back to old method where it's drawn at that size instead of __being__ that size
     for (let i = 0; i < filters.length; i++)
         GridManager.addCell(new Cell(img, filters[i]));
 }
