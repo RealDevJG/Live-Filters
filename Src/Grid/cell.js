da267144865401320d476d2fcf7d2022d25b1804
DevJG <andrew.georgiou03@gmail.com>
2024-01-30 18:55:33 +0000

    2D Grid Manager working

--- Src/Grid/cell.js
+++ Src/Grid/cell.js
@@ -1,8 +1,10 @@
+const scale = 1.6;
+
 class GridManager
 {
     static s_Grid = [];
-    static s_Width = 160;
-    static s_Height = 120;
+    static s_Width = 160 * scale;
+    static s_Height = 120 * scale;
 
     static addCell(_cell)
     {
@@ -12,20 +14,26 @@ class GridManager
     static drawCells()
     {
         for (let i = 0; i < this.s_Grid.length; ++i)
-            this.s_Grid[i].draw(i * this.s_Width, 0, this.s_Width, this.s_Height);
+        {
+            const x = (i * this.s_Width) % width;
+            const y = Math.floor(i * this.s_Width / width) * this.s_Height;
+
+            this.s_Grid[i].draw(x, y, this.s_Width, this.s_Height);
+        }
     }
 }
 
 class Cell
 {
-    constructor()
+    constructor(_img, _filter)
     {
         this.background = color(random(255), random(255), random(255));
+        this.img = _img;
+        this.filter = _filter;
     }
 
     draw(_x, _y, _width, _height)
     {
-        fill(this.background);
-        rect(_x, _y, _width, _height);
+        image(img, _x, _y, _width, _height);
     }
 }
--- Src/sketch.js
+++ Src/sketch.js
@@ -1,15 +1,23 @@
+let img;
+
+function preload()
+{
+    img = loadImage("Assets/test-image.jpg");
+}
+
 function setup()
 {
     createCanvas(1280, 720);
 
-    for (let i = 0; i < 8; ++i)
-        GridManager.addCell(new Cell());
+    for (let i = 0; i < 14; ++i)
+        GridManager.addCell(new Cell(img));
 }
 
 function draw()
 {
     background(125);
     GridManager.drawCells();
+    // image(img, 0, 0);
 }
 
 function keyPressed()
