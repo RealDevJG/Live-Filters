446e94de48564d571a5a7561ea83315a0823ee4e
DevJG <andrew.georgiou03@gmail.com>
2024-02-03 22:34:02 +0000

    Created reusable perPixel function

--- Src/Filters/greyscale.js
+++ Src/Filters/greyscale.js
@@ -1,21 +1,12 @@
 function greyscaleFilter(_img)
 {
-    _img.loadPixels();
-    for (let y = 0; y < _img.height; ++y)
+    perPixel(_img, (index) =>
     {
-        for (let x = 0; x < _img.width; ++x)
-        {
-            const index = (x + y * _img.width) * 4;
-            let average = ((_img.pixels[index + 0] + _img.pixels[index + 1] + _img.pixels[index + 2]) / 3) * 1.2;
+        const average = min(255, ((_img.pixels[index + 0] + _img.pixels[index + 1] + _img.pixels[index + 2]) / 3) * 1.2);
 
-            if (average > 255)
-                average = 255; 
-
-            _img.pixels[index + 0] = average;
-            _img.pixels[index + 1] = average;
-            _img.pixels[index + 2] = average;
-            _img.pixels[index + 3] = 255;
-        }
-    }
-    _img.updatePixels();
+        _img.pixels[index + 0] = average;
+        _img.pixels[index + 1] = average;
+        _img.pixels[index + 2] = average;
+        _img.pixels[index + 3] = 255;
+    });
 }
--- Src/Filters/maskChannel.js
+++ Src/Filters/maskChannel.js
@@ -2,27 +2,21 @@ function maskChannelFilter(mask)
 {
     return function(_img)
     {
-        _img.loadPixels();
-        for (let y = 0; y < _img.height; ++y)
+        perPixel(_img, (index) =>
         {
-            for (let x = 0; x < _img.width; ++x)
-            {
-                const index = (x + y * _img.width) * 4;
-                const r = _img.pixels[index + 0];
-                const g = _img.pixels[index + 1];
-                const b = _img.pixels[index + 2];
-                const a = _img.pixels[index + 3];
+            const r = _img.pixels[index + 0];
+            const g = _img.pixels[index + 1];
+            const b = _img.pixels[index + 2];
+            const a = _img.pixels[index + 3];
 
-                const uint32Colour = new Uint32Array(new ArrayBuffer(4));
-                uint32Colour[0] = (r << 24) | (g << 16) | (b << 8) | a;
+            const uint32Colour = new Uint32Array(new ArrayBuffer(4));
+            uint32Colour[0] = (r << 24) | (g << 16) | (b << 8) | a;
 
-                const maskedColour = uint32Colour[0] & mask;
-                _img.pixels[index + 0] = maskedColour >> 24 & 0xFF;
-                _img.pixels[index + 1] = maskedColour >> 16 & 0x00FF;
-                _img.pixels[index + 2] = maskedColour >> 8 & 0x0000FF;
-                _img.pixels[index + 3] = maskedColour & 0x000000FF;
-            }
-        }
-        _img.updatePixels();
+            const maskedColour = uint32Colour[0] & mask;
+            _img.pixels[index + 0] = maskedColour >> 24 & 0xFF;
+            _img.pixels[index + 1] = maskedColour >> 16 & 0x00FF;
+            _img.pixels[index + 2] = maskedColour >> 8 & 0x0000FF;
+            _img.pixels[index + 3] = maskedColour & 0x000000FF;
+        });
     }
 }
