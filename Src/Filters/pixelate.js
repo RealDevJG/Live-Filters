27c383325950fded95196675faa165c8a9d505eb
DevJG <andrew.georgiou03@gmail.com>
2024-02-07 00:26:24 +0000

    Implemented face recognition filters & adapted convolute function from lecture videos

--- Src/Filters/pixelate.js
+++ Src/Filters/pixelate.js
@@ -1,4 +1,40 @@
-function pixelateFilter()
+function pixelateFilter(_img)
 {
+    // greyscaleFilter(_img);
 
+    _img.loadPixels();
+    for (let y = 2; y < _img.height; y += 5)
+    {
+        for (let x = 2; x < _img.width; x += 5)
+        {
+            const tempImg = _img.get(x - 2, y - 2, 5, 5);
+            const colour = averageChannels(tempImg);
+
+            perPixel(tempImg, (_index, _x, _y) =>
+            {
+                const index = ((x + _x - 2) + (y + _y - 2) * _img.width) * 4;
+
+                _img.pixels[index + 0] = colour.r;
+                _img.pixels[index + 1] = colour.g;
+                _img.pixels[index + 2] = colour.b;
+            });
+        }
+    }
+    _img.updatePixels();
+}
+
+function averageChannels(_img)
+{
+    _img.loadPixels();
+
+    const colour = { r: 0, g: 0, b: 0 };
+
+    perPixel(_img, (index) =>
+    {
+        colour.r += _img.pixels[index + 0] / (_img.width * _img.height);
+        colour.g += _img.pixels[index + 1] / (_img.width * _img.height);
+        colour.b += _img.pixels[index + 2] / (_img.width * _img.height);
+    });
+
+    return colour;
 }
--- Src/utilities.js
+++ Src/utilities.js
@@ -1,12 +1,30 @@
-function convolute()
+function convolute(_img, _x, _y, _kernel)
 {
-    
+    const kernelSize = _kernel.length;
+    const offset = Math.floor(kernelSize / 2);
+
+    const colour = { r: 0, g: 0, b: 0 };
+    for (let y = 0; y < kernelSize; ++y)
+    {
+        for (let x = 0; x < kernelSize; ++x)
+        {
+            const pixelX = Math.max(0, Math.min(_x + x - offset, _img.width - 1));
+            const pixelY = Math.max(0, Math.min(_y + y - offset, _img.height - 1));
+
+            const convolutionIndex = (pixelX + pixelY * _img.width) * 4;
+            colour.r += _img.pixels[convolutionIndex + 0] * _kernel[y][x];
+            colour.g += _img.pixels[convolutionIndex + 1] * _kernel[y][x];
+            colour.b += _img.pixels[convolutionIndex + 2] * _kernel[y][x];
+        }
+    }
+
+    return colour;
 }
 
 function randomHex()
 {
     // 16777215 because that's what the max hex value for RGB (0xFFFFFF) excluding alpha is in decimal
-    return Number("0x" + Math.floor(Math.random()*16777215).toString(16) + "FF");
+    return Number("0x" + Math.floor(Math.random() * 16777215).toString(16) + "FF");
 }
 
 function perPixel(_img, _operation)
@@ -20,7 +38,7 @@ function perPixel(_img, _operation)
         for (let x = 0; x < _img.width; ++x)
         {
             const index = (x + y * _img.width) * 4;
-            _operation(index);
+            _operation(index, x, y);
         }
     }
 
